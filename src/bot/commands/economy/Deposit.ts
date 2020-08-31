b748bdfe5ad4382b19afff2464650a21de52cffc
aesthetical <57580886+Sxmurai@users.noreply.github.com>
2020-08-30 22:29:29 -0500

    bank related commands

---
+++ src/bot/commands/economy/Deposit.ts
@@ -0,0 +1,41 @@
+import { Message, MessageEmbed } from "discord.js";
+import { Command } from "discord-akairo";
+import { PublicCommand } from "#core";
+
+@PublicCommand("deposit", {
+  aliases: ["deposit"],
+  description: { content: "Puts money into the bank", usage: "[amount]" },
+  args: [
+    {
+      id: "amount",
+      type: "number",
+      prompt: {
+        start: "Please provide an amount to deposit",
+      },
+    },
+  ],
+})
+export default class DepositCommand extends Command {
+  public async exec(message: Message, { amount }: { amount: number }) {
+    const data = await message.member.economy();
+
+    if (data.wallet < amount || amount <= 0)
+      return message.util.send(
+        new MessageEmbed()
+          .setColor("#f55e53")
+          .setDescription(`I don't think you can put that into the bank`)
+      );
+
+    data.wallet -= amount;
+    data.bank += amount;
+    await data.save();
+
+    return message.util.send(
+      new MessageEmbed()
+        .setColor("#42f590")
+        .setDescription(
+          `Alright, I've deposited **${amount}¤** into your bank account`
+        )
+    );
+  }
+}
---
+++ src/bot/commands/economy/Withdraw.ts
@@ -0,0 +1,41 @@
+import { Message, MessageEmbed } from "discord.js";
+import { Command } from "discord-akairo";
+import { PublicCommand } from "#core";
+
+@PublicCommand("withdraw", {
+  aliases: ["withdraw"],
+  description: { content: "Takes money out of the bank", usage: "[amount]" },
+  args: [
+    {
+      id: "amount",
+      type: "number",
+      prompt: {
+        start: "Please provide an amount to withdraw",
+      },
+    },
+  ],
+})
+export default class WithdrawCommand extends Command {
+  public async exec(message: Message, { amount }: { amount: number }) {
+    const data = await message.member.economy();
+
+    if (data.bank < amount || amount <= 0)
+      return message.util.send(
+        new MessageEmbed()
+          .setColor("#f55e53")
+          .setDescription(`Seems that you don't have that much in the bank`)
+      );
+
+    data.wallet += amount;
+    data.bank -= amount;
+    await data.save();
+
+    return message.util.send(
+      new MessageEmbed()
+        .setColor("#42f590")
+        .setDescription(
+          `Alright, I've withdrawed **${amount}¤** out of your bank account`
+        )
+    );
+  }
+}
