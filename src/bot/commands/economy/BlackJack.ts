7b04316fffb759f853e1c1e289172b3b804f8c14
aesthetical <57580886+Sxmurai@users.noreply.github.com>
2020-09-02 15:30:18 -0500

    add command restrictions

--- src/bot/commands/economy/BlackJack.ts
+++ src/bot/commands/economy/BlackJack.ts
@@ -19,6 +19,7 @@ import { PublicCommand, BlackJack } from "#core";
       },
     },
   ],
+  channel: "guild"
 })
 export default class BlackJackCommand extends Command {
   public async exec(message: Message, { amount }: { amount: number }) {
