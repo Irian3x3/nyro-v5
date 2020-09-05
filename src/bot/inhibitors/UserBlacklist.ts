c7871643290d28914a11b31c08e2511df46314ea
aesthetical <57580886+Sxmurai@users.noreply.github.com>
2020-09-05 12:39:31 -0500

    blacklist inhibitor

---
+++ src/bot/inhibitors/UserBlacklist.ts
@@ -0,0 +1,12 @@
+import { Inhibitor } from "discord-akairo";
+import { Activator } from "#core";
+import { Message } from "discord.js";
+
+@Activator("blacklistUser", { reason: "blacklistedUser" })
+export default class uBlacklist extends Inhibitor {
+  public exec(message: Message): boolean {
+    return this.client.settings
+      .get<any[]>("global", "blacklist.users", [])
+      .some((key) => key.id === message.author.id);
+  }
+}
